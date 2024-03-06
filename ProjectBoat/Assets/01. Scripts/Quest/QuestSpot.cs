ddbef8f5f87e6d5240ac6dff6ef5a631c2894e48
SEH00N <06psh0501@gmail.com>
2024-03-07 00:13:52 +0900

    quest base

---
+++ ProjectBoat/Assets/01. Scripts/Quest/QuestSpot.cs
@@ -0,0 +1,40 @@
+using System;
+using UnityEngine;
+
+public class QuestSpot : MonoBehaviour, IInteractable
+{
+    private Quest currentQuest = null;
+
+    public event Action<StuffSO> OnQuestProcessEvent;
+
+    public void StartQuest(Quest quest)
+    {
+        currentQuest = quest;
+    }
+
+    public bool Interact(Component performer, bool actived, Vector3 point = default)
+    {
+        if(actived == false)
+            return false;
+
+        PlayerHand hand = (performer as PlayerInteractor)?.Hand;
+        if(hand == null || hand.IsEmpty)
+            return false;
+
+        Stuff holdingStuff = hand.HoldingObject as Stuff;
+        if (holdingStuff == null)
+            return false;
+
+        ProcessQuest(holdingStuff.StuffData);
+
+        hand.Release();
+        Destroy(holdingStuff.gameObject);
+
+        return true;
+    }
+
+    private void ProcessQuest(StuffSO stuffData)
+    {
+        OnQuestProcessEvent?.Invoke(stuffData);
+    }
+}
