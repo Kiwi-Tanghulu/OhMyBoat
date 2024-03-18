fd269547ba728640e8e24dd0c4f9586381ddfdca
kjs613 <ggm.kimjiseong613@gmail.com>
2024-03-19 00:33:37 +0900

    settlement boarding

---
+++ ProjectBoat/Assets/01. Scripts/Island/BoardingPoint.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardingPoint : MonoBehaviour
+{
+    private void Start()
+    {
+        Player.Instance.OnBoarding += Player_OnBoarding;
+        Ship.Instance.OnSettlemented += Ship_OnSettlemented;
+    }
+
+    private void Ship_OnSettlemented(Island island)
+    {
+        gameObject.SetActive(true);
+    }
+
+    private void Player_OnBoarding()
+    {
+        gameObject.SetActive(false);
+    }
+}
