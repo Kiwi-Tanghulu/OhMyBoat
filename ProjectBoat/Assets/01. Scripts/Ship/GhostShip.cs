5d4e235773c31cb0067830bcf67763a3eb3c2fec
JUNSUNG06 <forest2847@gmail.com>
2024-03-24 13:57:14 +0900

    ghost ship cannons

--- ProjectBoat/Assets/01. Scripts/Ship/GhostShip.cs
+++ ProjectBoat/Assets/01. Scripts/Ship/GhostShip.cs
@@ -1,16 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class GhostShip : MonoBehaviour
 {
+    [Space]
     private Animator anim;
     private readonly int appearHash = Animator.StringToHash("Appear");
     private readonly int disappearHash = Animator.StringToHash("Disappear");
 
+    [Space]
+    private Cannon[] cannons;
+    private int[] cannonIndices;
+
     private void Awake()
     {
         anim = GetComponent<Animator>();
+
+        cannons = transform.Find("Cannons").GetComponentsInChildren<Cannon>();
+        cannonIndices = new int[cannons.Length];
+        for (int i = 0; i < cannons.Length; i++)
+            cannonIndices[i] = i;
     }
 
     private void Update()
@@ -19,6 +30,8 @@ public class GhostShip : MonoBehaviour
             Appear(true);
         if (Input.GetKeyDown(KeyCode.D))
             Appear(false);
+        if (Input.GetKeyDown(KeyCode.E))
+            Fire(5);
     }
 
     public void Appear(bool value)
@@ -27,5 +40,25 @@ public class GhostShip : MonoBehaviour
             anim.SetTrigger(appearHash);
         else
             anim.SetTrigger(disappearHash);
+    }
+
+    public void Fire(int fireCount)
+    {
+        int shuffleCount = 10;
+        int a;
+        int b;
+        int temp;
+        for(int i = 0; i < shuffleCount; i++)
+        {
+            a = UnityEngine.Random.Range(0, cannonIndices.Length);
+            b = UnityEngine.Random.Range(0, cannonIndices.Length);
+            
+            temp = cannonIndices[a];
+            cannonIndices[a] = cannonIndices[b];
+            cannonIndices[b] = temp;
+        }
+
+        for (int i = 0; i < fireCount; i++)
+            cannons[cannonIndices[i]].Fire();
     }
 }
